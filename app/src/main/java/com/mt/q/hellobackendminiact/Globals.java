7b35a0849d9306958fa4ed94a52221ec4b0fd27c
QuanEnPan <q@MacBook-Air-de-QunLi.local>
2015-03-23 17:52:27 +0100

    Initial commit

---
+++ app/src/main/java/com/mt/q/hellobackendminiact/Globals.java
@@ -0,0 +1,18 @@
+package com.mt.q.hellobackendminiact;
+
+/**
+ * Created by q on 22/03/15.
+ */
+public class Globals
+{
+    public static final String TAG = "GCM DEMO";
+
+    public static final String GCM_SENDER_ID = "894705318036";
+    // id-2210
+    // 650116039430
+
+    public static final String PREFS_NAME = "GCM_DEMO";
+    public static final String PREFS_PROPERTY_REG_ID = "registration_id";
+
+    public static final long GCM_TIME_TO_LIVE = 60L * 60L * 24L * 7L * 4L; // 4 Weeks
+}
